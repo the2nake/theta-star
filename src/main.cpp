31249cc004f48ee04254b150fb65101d470c78e4
the2nake <41810559+the2nake@users.noreply.github.com>
2026-01-17 12:18:05 -0500

    chore: tidy demo test cases

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-#include <print>
-
-#include "pathfind.hpp"
-#include "thetastar.hpp"
-
 // TODO: figure out unit testing
 // handle exceptions:
 // * reaching max depth (circular trace)
@@ -10,17 +5,27 @@
 // * coordinate outside of grid
 // * wall found in trace
 
+#include <print>
+
+#include "pathfind.hpp"
+#include "thetastar.hpp"
+
 extern std::string maze;
 extern std::string block;
 
+void show(const pf::grid &g, const std::vector<pf::coord> &result) {
+  std::print("{}", pf::to_string(g, result));
+  std::println("path:\n{}", pf::to_string(result));
+}
+
 int main() {
   pf::grid g(10, 10);
-  g.load(maze);
 
   try {
-    auto result = pf::theta_star(g, {0, 2}, {9, 5});
-    std::print("{}", pf::to_string(g, result));
-    std::println("path:\n{}", pf::to_string(result));
+    for (auto &setup : {maze, block}) {
+      g.load(setup);
+      show(g, pf::theta_star(g, {0, 2}, {9, 5}));
+    }
   } catch (std::exception &e) { std::print("error: {}\n", e.what()); }
 }
 
